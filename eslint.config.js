f2b024baf0d2005276ece6a666baa4683bbcf8bd
Danrley Senegalha Pires <22007659+dansenpir@users.noreply.github.com>
2025-01-07 20:29:49 -0300

    chore: add eslint-plugin-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,10 @@
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import eslintPluginPrettier from "eslint-plugin-prettier";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
-  tseslint.configs.stylisticTypeChecked
+  tseslint.configs.stylisticTypeChecked,
+  eslintPluginPrettier.configs.recommended,
 );
