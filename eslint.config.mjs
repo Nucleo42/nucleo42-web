c4ab7317fd24887a1bf56bbedf877686df445371
Danrley Senegalha Pires <22007659+dansenpir@users.noreply.github.com>
2025-01-09 17:51:51 -0300

    chore: change eslint config extension to mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
+);
