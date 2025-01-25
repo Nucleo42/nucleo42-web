46cd62b4ff757d80a0e50ed10808a948cc5ae5cf
Ramiro-R2D2 <ricardo.rcostasilva@gmail.com>
2025-01-25 14:41:47 -0300

    chore(test): configure jest as testing library
    Configuração dos arquivos para a integração da biblioteca Jest como biblioteca de testes.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
-    ignores: ['.angular', '.vscode', 'node_modules', 'public', '**/*.mjs'],
+    ignores: ['.angular', '.vscode', 'node_modules', 'public', '**/*.mjs','setupJest.ts'],
   },
   {
     languageOptions: {
