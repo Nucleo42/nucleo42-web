e1c10c33cd6a6ab5fb40020ea0607756ea3feb1d
Ramiro-R2D2 <ricardo.rcostasilva@gmail.com>
2025-01-29 09:03:05 -0300

    chore(tests): configures ts-jest to use tsconfig specific to each test type

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,37 @@
-// eslint-disable-next-line no-undef
 module.exports = {
+
+  globals: {
+    'ts-jest': {
+      tsConfig: "<rootDir/tsconfig.json>"
+    }
+  },
   projects: [
     {
       displayName: "Unit Tests",
       testEnvironment: "jsdom",
       testMatch: ["**/src/tests/*.spec.ts"],
       transform: {
-        "^.+\\.tsx?$": "ts-jest"
-      },
+        "^.+\\.tsx?$": [
+          "ts-jest",
+          {
+            tsconfig: "<rootDir>/tsconfig.spec.json"
+          }
+        ]
+      }
     },
     {
       displayName: "Integration Tests",
       testEnvironment: "node",
       testMatch: ["**/src/tests/*.test.ts"],
       transform: {
-        "^.+\\.tsx?$": "ts-jest"
+        "^.+\\.tsx?$": [
+          "ts-jest",
+          {
+            tsconfig: "<rootDir>/tsconfig.test.json"
+          }
+        ]
       },
-      setupFilesAfterEnv: ["<rootDir>/jest-integration-config.js"]
-    },
-  ],
+      setupFilesAfterEnv: ["<rootDir>/jest-integration-config.js"],
+    }
+  ]
 };
