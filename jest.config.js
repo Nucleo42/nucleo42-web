2691aa53366a26aa8e387cda9bbfc775f7c26953
Ramiro-R2D2 <ricardo.rcostasilva@gmail.com>
2025-01-30 21:11:17 -0300

    chore(test): configure jest as testing library

---
+++ jest.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir/tsconfig.json>',
+    },
+  },
+  projects: [
+    {
+      displayName: 'Unit Tests',
+      testEnvironment: 'jsdom',
+      testMatch: ['**/src/tests/*.spec.ts'],
+      transform: {
+        '^.+\\.tsx?$': [
+          'ts-jest',
+          {
+            tsconfig: '<rootDir>/tsconfig.spec.json',
+          },
+        ],
+      },
+    },
+    {
+      displayName: 'Integration Tests',
+      testEnvironment: 'node',
+      testMatch: ['**/src/tests/*.test.ts'],
+      transform: {
+        '^.+\\.tsx?$': [
+          'ts-jest',
+          {
+            tsconfig: '<rootDir>/tsconfig.test.json',
+          },
+        ],
+      },
+      setupFilesAfterEnv: ['<rootDir>/jest-integration-config.js'],
+    },
+  ],
+};
