78169ca52dbcc980185505ca7f8245cc540b5537
Ramiro-R2D2 <ricardo.rcostasilva@gmail.com>
2025-01-31 04:34:50 -0300

    chore(tests): create a specific Jest configuration file for unit tests

---
+++ jest-unit-config.js
@@ -0,0 +1,6 @@
+export const displayName = 'Unit Tests';
+export const testEnvironment = 'jsdom';
+export const testMatch = ['**/src/tests/*.spec.ts'];
+export const transform = {
+  '^.+\\.tsx?$': 'ts-jest',
+};
