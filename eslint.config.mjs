986d1b15c462396b1a8c6de50088a8278722fe69
Danrley Senegalha Pires <22007659+dansenpir@users.noreply.github.com>
2025-02-02 11:49:29 -0300

    chore: remove unnecessary dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import importPlugin from 'eslint-plugin-import';
 
 export default tseslint.config(
   {
@@ -14,18 +13,10 @@ export default tseslint.config(
         tsconfigRootDir: import.meta.dirname,
       },
     },
-    settings: {
-      'import/resolver': {
-        typescript: {
-          project: './tsconfig.json',
-        },
-      },
-    },
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.strictTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
-  importPlugin.configs.recommended,
   eslintConfigPrettier,
 );
