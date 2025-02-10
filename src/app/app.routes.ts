8c469fcf85fb6277ba85669bc7a6704121cb436f
João Pedro <joaobuenoh@hotmail.com>
2025-02-09 22:07:48 -0300

    refactor: changes route config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
+import { LayoutComponent } from './components/layout/layout.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
 
-  { path: '**', component: LoginComponent },
+  { path: 'layout', component: LayoutComponent },
+
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
 ];
