bd56f1a25fb9cf460a784a391d06cbcc870ce142
João Pedro <joaobuenoh@hotmail.com>
2025-02-16 10:54:07 -0300

    refactor: remove comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, LayoutComponent, RouterOutlet],
+  imports: [HeaderComponent, LayoutComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
