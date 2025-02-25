63c562f1565c6bd688d3a4cb6dbabea83593dbb2
João Pedro <joaobuenoh@hotmail.com>
2025-02-24 22:04:22 -0300

    test: achieve 100% coverage

--- src/app/components/footer/footer.component.spec.ts
+++ src/app/components/footer/footer.component.spec.ts
@@ -2,16 +2,32 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
 import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
+  let iconRegistry: MatIconRegistry;
+  let sanitizer: DomSanitizer;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FooterComponent],
-      providers: [provideHttpClient(), provideHttpClientTesting()],
+      imports: [MatIconModule, FooterComponent],
+      providers: [provideHttpClient(), provideHttpClientTesting(), MatIconRegistry],
     }).compileComponents();
+
+    iconRegistry = TestBed.inject(MatIconRegistry);
+    sanitizer = TestBed.inject(DomSanitizer);
+    iconRegistry.addSvgIconLiteral('linkedin', sanitizer.bypassSecurityTrustHtml('<svg></svg>'));
+
+    iconRegistry = TestBed.inject(MatIconRegistry);
+    sanitizer = TestBed.inject(DomSanitizer);
+    iconRegistry.addSvgIconLiteral('github', sanitizer.bypassSecurityTrustHtml('<svg></svg>'));
+
+    iconRegistry = TestBed.inject(MatIconRegistry);
+    sanitizer = TestBed.inject(DomSanitizer);
+    iconRegistry.addSvgIconLiteral('discord', sanitizer.bypassSecurityTrustHtml('<svg></svg>'));
 
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
@@ -23,16 +39,28 @@ describe('FooterComponent', () => {
   });
 
   describe('handleClick() Method', () => {
-    it('should open Twitter URL for platform "twitter"', () => {
-      const openSpy = jest.spyOn(window, 'open').mockImplementation(() => null);
-      component.handleClick('twitter');
-      expect(openSpy).toHaveBeenCalledWith('https://twitter.com', '_blank');
-      component.handleClick('linkedIn');
-      expect(openSpy).toHaveBeenCalledWith('https://twitter.com', '_blank');
-      component.handleClick('gitHub');
-      expect(openSpy).toHaveBeenCalledWith('https://twitter.com', '_blank');
+    it('should open LinkedIn URL for platform "linkedIn"', () => {
+      const openSpy = jest.spyOn(window, 'open').mockImplementation(jest.fn());
+      component.handleClick('linkedin');
+      expect(openSpy).toHaveBeenCalledWith('https://linkedin.com', '_blank');
+    });
+
+    it('should open GitHub URL for platform "gitHub"', () => {
+      const openSpy = jest.spyOn(window, 'open').mockImplementation(jest.fn());
+      component.handleClick('github');
+      expect(openSpy).toHaveBeenCalledWith('https://github.com', '_blank');
+    });
+
+    it('should open Discord URL for platform "discord"', () => {
+      const openSpy = jest.spyOn(window, 'open').mockImplementation(jest.fn());
       component.handleClick('discord');
-      expect(openSpy).toHaveBeenCalledWith('https://twitter.com', '_blank');
+      expect(openSpy).toHaveBeenCalledWith('https://discord.com', '_blank');
+    });
+
+    it('should log "Unknown platform" for unknown platform', () => {
+      const logSpy = jest.spyOn(console, 'log').mockImplementation(jest.fn());
+      component.handleClick('unknown');
+      expect(logSpy).toHaveBeenCalledWith('Unknown platform');
     });
   });
 });
