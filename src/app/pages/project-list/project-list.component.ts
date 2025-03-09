c419baa782416351fd1a4e2d16cdaefdc5a93b51
Ramiro-R2D2 <ricardo.rcostasilva@gmail.com>
2025-03-09 00:43:33 -0300

    refactor: remove unused NavBarComponent import

--- src/app/pages/project-list/project-list.component.ts
+++ src/app/pages/project-list/project-list.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { NavBarComponent } from '../../components/nav-bar/nav-bar.component';
 import { FilterComponent } from '../../components/filter/filter.component';
 import { ProjectCardComponent } from '../../components/project-card/project-card.component';
 
 @Component({
   selector: 'app-project-list',
-  imports: [NavBarComponent, FilterComponent, ProjectCardComponent],
+  imports: [FilterComponent, ProjectCardComponent],
   standalone: true,
   templateUrl: './project-list.component.html',
   styleUrl: './project-list.component.scss',
