be4d5ef91d22100345f1ad412424964ab85d88b7
Ramiro-R2D2 <ricardo.rcostasilva@gmail.com>
2025-03-09 06:21:30 -0300

    refactor: switch to mat-icon for search and chevron buttons

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 import { MenuBurgerComponent } from '@app/components/menu-burger/menu-burger.component';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [MenuBurgerComponent],
+  imports: [MatIconModule, MenuBurgerComponent],
+  providers: [MatIconRegistry],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
 export class NavBarComponent {
-  title = '';
+  constructor(
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer,
+  ) {
+    this.matIconRegistry.addSvgIcon('search', this.domSanitizer.bypassSecurityTrustResourceUrl('icons/search.svg'));
+    this.matIconRegistry.addSvgIcon('chevron', this.domSanitizer.bypassSecurityTrustResourceUrl('icons/chevron.svg'));
+  }
 }
