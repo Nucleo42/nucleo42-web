20689bbe3b305934d5bac642b4e0785c9f461162
Danrley Senegalha Pires <22007659+dansenpir@users.noreply.github.com>
2025-03-19 20:29:50 -0300

    chore: setup jest to fail if tests not cover 90%

--- jest.config.js
+++ jest.config.js
@@ -22,10 +22,10 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageThreshold: {
     global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90,
     },
   },
   roots: ['<rootDir>/src', '<rootDir>/tests'],
