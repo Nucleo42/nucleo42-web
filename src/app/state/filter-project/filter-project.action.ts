8b68d10e5327a9461691ce995b4733f58ee0c563
Ramiro-R2D2 <ricardo.rcostasilva@gmail.com>
2025-05-03 02:56:07 -0300

    feat(filters): support multiple values for status and difficulty filters

--- src/app/state/filter-project/filter-project.action.ts
+++ src/app/state/filter-project/filter-project.action.ts
@@ -25,12 +25,12 @@ export class ApplyStackFilter {
 
 export class ApplyStatusFilter {
   static readonly type = '[Filters] Apply Status';
-  constructor(public payload: StatusProjectEnum) {}
+  constructor(public payload: StatusProjectEnum[]) {}
 }
 
 export class ApplyDifficultyFilter {
   static readonly type = '[Filters] Apply Difficulty';
-  constructor(public payload: DifficultyLevel) {}
+  constructor(public payload: DifficultyLevel[]) {}
 }
 
 export class ApplyJobStatusFilter {
